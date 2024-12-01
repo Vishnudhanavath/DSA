dfc5f9b402b5332b339fe86192e245ff0f3cd8f1
Vishnudhanavath <86546017+Vishnudhanavath@users.noreply.github.com>
2024-12-01 11:48:35 +0530

    Create FlattenBinaryTreeToLinkedList.cpp
    it is a preorder traversal

---
+++ BinaryTree/FlattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode*prev = nullptr;
+
+    void createLinkedList(TreeNode* curr){
+        if(!curr) return;
+        if(prev){
+            prev ->left= nullptr;
+            prev ->right = curr;
+            
+        }
+        TreeNode* r = curr->right;
+        prev = curr;
+        createLinkedList(curr->left);
+        createLinkedList(r);
+    }
+
+
+    void flatten(TreeNode* root) {
+        createLinkedList(root);
+    }
+};
