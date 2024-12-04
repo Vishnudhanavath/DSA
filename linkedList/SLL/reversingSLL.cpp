bdf18fe36fb75f4dbac6ba9e43bcd79a0da32b70
Vishnudhanavath <86546017+Vishnudhanavath@users.noreply.github.com>
2024-12-04 22:03:07 +0530

    Create reversingSLL.cpp

---
+++ linkedList/SLL/reversingSLL.cpp
@@ -0,0 +1,38 @@
+// BruteForce approach is pushing to stack and again using the previous linkedList
+// tc:= O(n) , sc:= O(n)
+//==================================
+//better method is:
+class Solution {
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head-> next == nullptr) return head;
+        ListNode* back = nullptr;
+        ListNode* front = head-> next;
+        while(head){
+            head -> next = back;
+            back = head;
+            head = front;
+            if(front != nullptr){
+            front = front->next;
+            }
+        }
+        return back;
+    }
+};
+
+
+// optimal methods :
+ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head ->next == nullptr) return head;
+        ListNode* prev = nullptr;
+        while(head){
+            ListNode* front = head->next;
+            head -> next = prev;
+            prev = head;
+            head = front;
+        }
+        return prev;
+    }
+};
+
